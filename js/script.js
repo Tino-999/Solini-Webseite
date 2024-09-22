4f99620f817a20ccdaefc0ee4199e41f2fd9f0cf
Tino-999 <tinoschuldt100@gmail.com>
2024-09-22 18:07:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,84 +6,78 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 20,
 }).addTo(map);
 
-/* Füge eine Satelliten-Kachel-Ebene hinzu (Mapbox) */
-// Mapbox Access Token vom Server abrufen
-fetch('/api/mapbox-token')
-    .then(response => response.json())
-    .then(data => {
-        var satelliteLayer = L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=${data.accessToken}`, {
-            maxZoom: 19,
-            tileSize: 512,
-            zoomOffset: -1,
-            attribution: '© Mapbox © OpenStreetMap'
-        }).addTo(map);
-    })
-    .catch(error => {
-        console.error('Fehler beim Abrufen des Mapbox-Tokens:', error);
-    });
+/* Satelliten-Kachel-Ebene hinzufügen (Mapbox) */
+// Direkte Zuweisung des Mapbox Access Tokens
+var mapboxAccessToken = 'pk.eyJ1IjoidGlubzk5OSIsImEiOiJjbTBjcWZuZm4wNHgzMmtzZHRzMWo1dnIxIn0.VkbQqLZUSE3G1xRhmyc94g'; // <-- Ersetzen Sie dies durch Ihren tatsächlichen Mapbox Access Token
+
+var satelliteLayer = L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/satellite-v12/tiles/{z}/{x}/{y}?access_token=${mapboxAccessToken}`, {
+    maxZoom: 19,
+    tileSize: 512,
+    zoomOffset: -1,
+    attribution: '© Mapbox © OpenStreetMap'
+}).addTo(map);
 
 /* ShadeMap setup */
-// ShadeMap API-Key vom Server abrufen
+// Direkte Zuweisung des ShadeMap API-Schlüssels
+var shadeMapApiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRpbm9zY2h1bGR0MTAwQGdtYWlsLmNvbSIsImNyZWF0ZWQiOjE3MjQ3MDY3Nzg3MjAsImlhdCI6MTcyNDcwNjc3OH0.PVdyHPNZ4UFD9w_yc8_3QEAjEVEutB2QFV4rUipHGuY'; // <-- Ersetzen Sie dies durch Ihren tatsächlichen ShadeMap API Key
+
 let shadeMap;
-fetch('/api/shademap-key')
-    .then(response => response.json())
-    .then(data => {
-        const loaderEl = document.getElementById('loader');
-        let now = new Date();
+try {
+    const loaderEl = document.getElementById('loader');
+    let now = new Date();
 
-        shadeMap = L.shadeMap({
-            apiKey: data.apiKey,
-            date: now,
-            color: '#7a6a6e',
-            opacity: 0.7,
-            terrainSource: {
-                maxZoom: 15,
-                tileSize: 256,
-                getSourceUrl: ({ x, y, z }) => `https://s3.amazonaws.com/elevation-tiles-prod/terrarium/${z}/${x}/${y}.png`,
-                getElevation: ({ r, g, b, a }) => (r * 256 + g + b / 256) - 32768,
-                _overzoom: 18,
-            },
-            getFeatures: async () => {
-                try {
-                    if (map.getZoom() > 15) {
-                        const bounds = map.getBounds();
-                        const north = bounds.getNorth();
-                        const south = bounds.getSouth();
-                        const east = bounds.getEast();
-                        const west = bounds.getWest();
-                        const query = `https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9Cbuilding%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cbuilding%E2%80%9D%0A%20%20way%5B%22building%22%5D%28${south}%2C${west}%2C${north}%2C${east}%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B`;
-                        const response = await fetch(query)
-                        const json = await response.json();
-                        const geojson = osmtogeojson(json);
-                        // If no building height, default to one storey of 3 meters
-                        geojson.features.forEach(feature => {
-                            if (!feature.properties) {
-                                feature.properties = {};
-                            }
-                            if (!feature.properties.height) {
-                                feature.properties.height = 3;
-                            }
-                        });
-                        return geojson.features;
-                    }
-                } catch (e) {
-                    console.error(e);
+    shadeMap = L.shadeMap({
+        apiKey: shadeMapApiKey,
+        date: now,
+        color: '#7a6a6e',
+        opacity: 0.7,
+        terrainSource: {
+            maxZoom: 15,
+            tileSize: 256,
+            getSourceUrl: ({ x, y, z }) => `https://s3.amazonaws.com/elevation-tiles-prod/terrarium/${z}/${x}/${y}.png`,
+            getElevation: ({ r, g, b, a }) => (r * 256 + g + b / 256) - 32768,
+            _overzoom: 18,
+        },
+        getFeatures: async () => {
+            try {
+                if (map.getZoom() > 15) {
+                    const bounds = map.getBounds();
+                    const north = bounds.getNorth();
+                    const south = bounds.getSouth();
+                    const east = bounds.getEast();
+                    const west = bounds.getWest();
+                    const query = `https://overpass-api.de/api/interpreter?data=%2F*%0AThis%20has%20been%20generated%20by%20the%20overpass-turbo%20wizard.%0AThe%20original%20search%20was%3A%0A%E2%80%9Cbuilding%E2%80%9D%0A*%2F%0A%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0A%2F%2F%20gather%20results%0A%28%0A%20%20%2F%2F%20query%20part%20for%3A%20%E2%80%9Cbuilding%E2%80%9D%0A%20%20way%5B%22building%22%5D%28${south}%2C${west}%2C${north}%2C${east}%29%3B%0A%29%3B%0A%2F%2F%20print%20results%0Aout%20body%3B%0A%3E%3B%0Aout%20skel%20qt%3B`;
+                    const response = await fetch(query);
+                    const json = await response.json();
+                    const geojson = osmtogeojson(json);
+                    // Wenn keine Gebäudehöhe vorhanden ist, standardmäßig 3 Meter setzen
+                    geojson.features.forEach(feature => {
+                        if (!feature.properties) {
+                            feature.properties = {};
+                        }
+                        if (!feature.properties.height) {
+                            feature.properties.height = 3;
+                        }
+                    });
+                    return geojson.features;
                 }
-                return [];
-            },
-            debug: (msg) => { console.log(new Date().toISOString(), msg) }
-        }).addTo(map);
+            } catch (e) {
+                console.error(e);
+            }
+            return [];
+        },
+        debug: (msg) => { console.log(new Date().toISOString(), msg) }
+    }).addTo(map);
 
-        shadeMap.on('tileloaded', (loadedTiles, totalTiles) => {
-            loaderEl.innerText = `Loading: ${(loadedTiles / totalTiles * 100).toFixed(0)}%`;
-        });
-
-        // Initiale Stunde auf der Karte einstellen
-        updateMapShadow(now.getHours());
-    })
-    .catch(error => {
-        console.error('Fehler beim Abrufen des ShadeMap API-Schlüssels:', error);
+    shadeMap.on('tileloaded', (loadedTiles, totalTiles) => {
+        loaderEl.innerText = `Loading: ${(loadedTiles / totalTiles * 100).toFixed(0)}%`;
     });
+
+    // Initiale Stunde auf der Karte einstellen
+    updateMapShadow(now.getHours());
+} catch (error) {
+    console.error('Fehler beim Initialisieren von ShadeMap:', error);
+}
 
 /* Controls setup */
 let intervalTimer;
